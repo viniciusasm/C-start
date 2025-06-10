2b727f5318369e4b33093824a381c3e14823ceda
vinicius <vini.asmd@outlook.com>
2025-06-10 13:36:26 -0300

    aula 2

---
+++ a2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace a2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static void Run()
+        {
+            PrintHelloWorld();
+            Console.ReadLine();
+        }
+
+        static void PrintHelloWorld()
+        {
+            Console.WriteLine("Olá mundo ...");
+        }
+    }
+}
+
+// This is a simple C# program that prints "Olá mundo ..." to the console.
